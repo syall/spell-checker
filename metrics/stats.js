7bec7ed97587cc30bd98d9d77db413d93201a10c
syall <s.yuan.all@gmail.com>
2020-07-13 04:13:51 -0400

    Update metrics

--- metrics/stats.js
+++ metrics/stats.js
@@ -4,6 +4,7 @@ const sc = SpellCheckerWithDirData();
 const arraySc = Array.from(sc.dictionary);
 
 console.log(`Metrics Report:`);
+
 const top = 10;
 const tw = arraySc.sort(([_1, v1], [_2, v2]) => v2 - v1).slice(0, top);
 const tp = tw.map(([k, _]) => sc.getProbability(k));
@@ -16,7 +17,9 @@ for (let i = 0; i < top; i++) {
     console.log(`- ${word}: ${count} at ${cent}`);
 }
 console.groupEnd();
+
 const uniqueWordCount = sc.dictionary.size;
 console.log(`- Unique Word Count: ${uniqueWordCount.toLocaleString()}`);
+
 const totalWordCount = arraySc.reduce((t, [_, v1]) => t + v1, 0);
 console.log(`- Total Words Scanned: ${totalWordCount.toLocaleString()}`);
